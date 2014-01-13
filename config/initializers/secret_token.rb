dc8a1d90a2967b2b77b534bef869b46ea810b7fc
Punit Lad <plad@thoughtworks.com>
2014-01-13 14:17:29 -0500

    generate rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Snackstack::Application.config.secret_key_base = '930be2abf3e779af1d93389bcde805a8553e248d938b5190731fcc7129738374fccb60add86bdd26ed1c55b94eee0d177bdb92d03d99eb3177a908b021e394e7'
